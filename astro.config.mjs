5566a2702bbab8458b2019f8655a4ad3d3eead6e
Marlonleon <marlonestebanleonrojas@gmail.com>
2024-06-03 20:58:14 -0500

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Marlonleon2023.github.io',
+    base: 'espiritual.github.io',
+});
